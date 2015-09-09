8fd673145a48d3dbca5151ece3cdd01491733052
VovoLurdes <flaviog.info@gmail.com>
2015-09-09 18:31:11 -0300

    SavedInstacend

--- app/src/main/java/br/com/teajuda/teajuda/CriarRotina.java
+++ app/src/main/java/br/com/teajuda/teajuda/CriarRotina.java
@@ -21,7 +21,6 @@ public class CriarRotina extends ActionBarActivity {
 
     AudioRecord voice = new AudioRecord();
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
